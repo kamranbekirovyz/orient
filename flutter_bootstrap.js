347688fac85d137817612e571758cce00117e046
kamranbekirovyz <kamranbekirovyz@users.noreply.github.com>
2026-02-08 14:31:17 +0000

    deploy: 1025cebabaff1cc545ffc54aa0b9db6383e302fd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1897734105"
+    serviceWorkerVersion: "2032862823"
   }
 });
